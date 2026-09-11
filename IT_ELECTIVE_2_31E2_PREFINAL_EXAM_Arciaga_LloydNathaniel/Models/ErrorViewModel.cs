8b6bfb06212cdd6d9b90c4308e7d3833aa4b74cc
lloydnathanielarciaga <lloydnathanielarciaga@gmail.com>
2026-09-11 09:16:36 +0800

    Add project files.

---
+++ IT_ELECTIVE_2_31E2_PREFINAL_EXAM_Arciaga_LloydNathaniel/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IT_ELECTIVE_2_31E2_PREFINAL_EXAM_Arciaga_LloydNathaniel.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
